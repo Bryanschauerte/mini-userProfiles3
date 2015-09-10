6663382c0432395ec13b29b030a14178d0585b61
Bryan Schauerte <bryan.schauerte@gmail.com>
2015-09-10 13:35:36 -0600

    promises initiated

--- service.js
+++ service.js
@@ -1,12 +1,28 @@
 var app = angular.module('userProfiles');
 
-app.service('mainService', function($http) {
+app.service('mainService', function($http, $q) {
 
   this.getUsers = function() {
-    return $http({
+    var deferred = $q.defer();
+      //returns an object with .then --all promise have .then()
+
+     $http({
         method: 'GET',
         url: 'http://reqr.es/api/users?page=1'
-    })
+    }).then(function(response){
+      var parsedResponse = response.data.data;
+
+      for(var i = 0; i < parsedResponse.length; i++) {
+        parsedResponse[i].first_name = 'Ralf'
+      }
+
+      //below tells us to run the sucess of the then() methods
+      deferred.resolve(parsedResponse)
+    });
+    //an object of deferred - promise has the .then method
+    return deferred.promise;
+
+
   }
 
 });
